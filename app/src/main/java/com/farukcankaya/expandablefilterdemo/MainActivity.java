be32cb3112aac5e498bfa4fb27463cedde4aec6d
Faruk Cankaya <farukcankayaaa@gmail.com>
2017-05-28 19:26:54 +0300

    add foursquare like filter menu example

--- app/src/main/java/com/farukcankaya/expandablefilterdemo/MainActivity.java
+++ app/src/main/java/com/farukcankaya/expandablefilterdemo/MainActivity.java
@@ -3,39 +3,36 @@ package com.farukcankaya.expandablefilterdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.farukcankaya.expandablefilter.ExpandableFilter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ExpandableFilter.OnItemSelectListener {
+    ExpandableFilter priceFilter, distanceFilter, openingFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ExpandableFilter expandableFilter = (ExpandableFilter) findViewById(R.id.expandableLayout);
-        List<String> items = new ArrayList<>(0);
-        items.add("\ue902");
-        items.add("\ue900");
-        items.add("\ue901");
-        expandableFilter.setEmoji("\ue902");
-        expandableFilter.setLabel("Price");
-        expandableFilter.setItems(items);
-        expandableFilter.setItemSelectListener(new ExpandableFilter.OnItemSelectListener() {
-            @Override
-            public void onSelected(int position, String text) {
-                Log.i("Select", "position:" + position + " text:" + text);
-            }
+        priceFilter = (ExpandableFilter) findViewById(R.id.priceFilter);
+        distanceFilter = (ExpandableFilter) findViewById(R.id.distanceFilter);
+        openingFilter = (ExpandableFilter) findViewById(R.id.openingFilter);
 
-            @Override
-            public void onDeselected(int position, String text) {
-                Log.i("Delect", "position:" + position + " text:" + text);
-            }
-        });
-        expandableFilter.toggle();
-        expandableFilter.selectItemAt(1);
-        expandableFilter.setDuration(800);
+        priceFilter.setItemSelectListener(this);
+        distanceFilter.setItemSelectListener(this);
+        openingFilter.setItemSelectListener(this);
+    }
+
+    @Override
+    public void onSelected(int position, String text) {
+        Toast.makeText(getApplicationContext(), "Selected: " + text + " Position:" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDeselected(int position, String text) {
+        Toast.makeText(getApplicationContext(), "Deselected: " + text + " Position:" + position, Toast.LENGTH_SHORT).show();
     }
 }
